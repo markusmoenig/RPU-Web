2c321e8b44e0d2ae9793d0edcc84a244dc0576b5
Markus Moenig <markus@moenig.io>
2024-05-13 20:00:56 +0700

    Update

--- content/news/_index.md
+++ content/news/_index.md
@@ -3,4 +3,6 @@ title = "News"
 weight = 1
 +++
 
-### Initial Release (v0.3.0) - xx of May 2024
+### Initial Release (v0.3.0) - 13th of May 2024
+
+The initial release of the project is now available.
--- public/index.search.js
+++ public/index.search.js
@@ -1,6 +1,6 @@
 var relearn_search_index = [
   {
-    "content": "Initial Release (v0.3.0) - xx of May 2024 ",
+    "content": "Initial Release (v0.3.0) - 13th of May 2024 The initial release of the project is now available.\n",
     "description": "",
     "tags": null,
     "title": "News",
